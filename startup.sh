bb5d14b894a8c9b8cc39ac3a44cc06d960c5a8c1
abdullahek <30905026+abdullahek@users.noreply.github.com>
2024-08-26 23:46:23 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get install -y unixodbc-dev
-ACCEPT_EULA=Y apt-get install msodbcsql17
+ACCEPT_EULA=Y apt-get install msodbcsql18
 gunicorn --bind=0.0.0.0 --timeout 600 manage:app
